185f8213060932bc39d9f046811f70b700991be7
denis-ttk-1975 <denis.ttk@gmail.com>
2023-07-20 22:02:39 +0300

    invalidate data added for queries

--- src/hooks/useAddNewTodoQuery.ts
+++ src/hooks/useAddNewTodoQuery.ts
@@ -2,6 +2,7 @@ import { useToast } from '@chakra-ui/react';
 import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query';
 import { fetchTodos, createTodo } from '../services/todos';
 import { TodoState } from '../types/todo';
+import { Todo } from '../types/todo';
 
 const useAddNewTodoQuery = (title: string) => {
   const toast = useToast();
@@ -9,6 +10,16 @@ const useAddNewTodoQuery = (title: string) => {
 
   return useMutation({
     mutationFn: () => createTodo(title),
+    onSuccess: (newTodo) => {
+      // client.getQueryData(['todos', 'all'])
+      client.setQueriesData<Todo[]>(['todos'], (oldTodos) => {
+        return [...((oldTodos as Todo[]) || []), newTodo];
+      });
+      client.invalidateQueries({
+        queryKey: ['todos'],
+        refetchType: 'none',
+      });
+    },
     onError: (err) => {
       if (err instanceof Error) {
         toast({
--- src/hooks/useToggleStatusTodoQuery.ts
+++ src/hooks/useToggleStatusTodoQuery.ts
@@ -2,6 +2,7 @@ import { useToast } from '@chakra-ui/react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { fetchTodos, toggleTodoStatus } from '../services/todos';
 import { TodoState } from '../types/todo';
+import { Todo } from '../types/todo';
 
 const useToggleStatusTodoQuery = (id: number, completed: boolean) => {
   const toast = useToast();
@@ -9,7 +10,20 @@ const useToggleStatusTodoQuery = (id: number, completed: boolean) => {
 
   return useMutation({
     mutationFn: () => toggleTodoStatus(id, !completed),
+    onSuccess: (newTodo) => {
+      // client.setQueriesData<Todo[]>(['todos'], (oldTodos) => {
+      //   if (oldTodos) {
+      //     console.log('oldTodos: ', oldTodos);
 
+      //     oldTodos[id - 1] = newTodo;
+      //   }
+      //   return [...((oldTodos as Todo[]) || [])];
+      // });
+      client.invalidateQueries({
+        queryKey: ['todos'],
+        // refetchType: 'none',
+      });
+    },
     onError: (err) => {
       if (err instanceof Error) {
         toast({
